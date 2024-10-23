388588dbafc055d39580efefe8b04d9ccf4269f5
Kamogelo Pitjeng <ST10198658@rcconnect.edu.za>
2024-10-23 12:12:19 +0000

    Create Lecturer Model

--- ContractMonthlyClaimSystem/Models/Lecturer.cs
+++ ContractMonthlyClaimSystem/Models/Lecturer.cs
@@ -5,5 +5,18 @@
         public int Id { get; set; }
 
         public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateofBirth { get; set; }
+
+        public int Phone { get; set; }
+        public string ModuleCode { get; set; }
+        public string ModuleName { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        
     }
 }
